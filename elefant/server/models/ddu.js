34ded260e54e41d1c8bf4e0587a9997e0fb6e852
iplus <ivan.plyusnin@gmail.com>
2017-08-27 22:53:04 +0300

    accept ddu by developer

--- elefant/server/models/ddu.js
+++ elefant/server/models/ddu.js
@@ -1,5 +1,18 @@
 'use strict';
 
-module.exports = function(Ddu) {
-
+module.exports = function (Ddu) {
+    Ddu.accept_by_developer = function (ddu_id, cb) {
+        // post to BC
+        // update etherium_id of DDU
+        cb(null, ddu_id);
+    };
+    Ddu.remoteMethod(
+        'accept_by_developer',
+        {
+            accepts: [{arg: 'ddu_id', type: 'string'}],
+            returns: {arg: 'result', type: 'string'},
+            http: {path: '/accept-by-developer', verb: 'post'},
+            description: "Accept DDU by developer and post it to blockchain"
+        }
+    );
 };
